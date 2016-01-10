169e3f7898c7fbff88a0a579474d21763204f4fd
Stephen Tu <stephent@berkeley.edu>
2016-01-10 12:26:10 -0800

    add both original featurizer alongside augmenter

--- src/main/scala/pipelines/images/cifar/RandomPatchCifarFeaturizerWithAugmentation.scala
+++ src/main/scala/pipelines/images/cifar/RandomPatchCifarFeaturizerWithAugmentation.scala
@@ -3,16 +3,29 @@ package pipelines.images.cifar
 import breeze.linalg._
 import breeze.numerics._
 import evaluation.MulticlassClassifierEvaluator
-import loaders.CifarLoader
+import loaders.GeneralCifarLoader
 import nodes.images._
 import nodes.learning.{BlockLeastSquaresEstimator, ZCAWhitener, ZCAWhitenerEstimator}
 import nodes.stats.{StandardScaler, Sampler}
 import nodes.util.{Cacher, ClassLabelIndicatorsFromIntLabels, MaxClassifier}
+import pipelines.FunctionNode
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.rdd.RDD
 import pipelines.Logging
 import scopt.OptionParser
-import utils.{MatrixUtils, Stats}
+import utils.{MatrixUtils, Stats, Image, ImageUtils}
 
+
+class LabelAugmenter(mult: Int) extends FunctionNode[RDD[Int], RDD[Int]] {
+  def apply(in: RDD[Int]) = in.flatMap(x => Seq.fill(mult)(x))
+}
+
+class ImageCrop(xStart: Int, yStart: Int,
+                xEnd: Int, yEnd: Int) extends workflow.Transformer[Image, Image] {
+
+  def apply(img: Image): Image = ImageUtils.crop(img, xStart, yStart, xEnd, yEnd)
+
+}
 
 object RandomPatchCifarFeaturizer extends Serializable with Logging {
   val appName = "RandomPatchCifarFeaturizer"
@@ -20,12 +33,14 @@ object RandomPatchCifarFeaturizer extends Serializable with Logging {
   def run(sc: SparkContext, conf: RandomCifarFeaturizerConfig) {
     //Set up some constants.
     val numClasses = 10
-    val imageSize = 32
+    val imageSize = conf.imageSize
     val numChannels = 3
     val whitenerSize = 100000
 
+    val loader = new GeneralCifarLoader(imageSize, imageSize)
+
     // Load up training data, and optionally sample.
-    val trainData = CifarLoader(sc, conf.trainLocation).cache
+    val trainData = loader(sc, conf.trainLocation).cache()
     val trainImages = ImageExtractor(trainData)
 
     val patchExtractor = new Windower(conf.patchSteps, conf.patchSize)
@@ -46,21 +61,29 @@ object RandomPatchCifarFeaturizer extends Serializable with Logging {
         ((unnormFilters(::, *) / (twoNorms + 1e-10)) * whitener.whitener.t, whitener)
     }
 
-    val unscaledFeaturizer = new Convolver(filters, imageSize, imageSize, numChannels, Some(whitener), true)
-        .andThen(SymmetricRectifier(alpha=conf.alpha))
+    val convolver = new Convolver(filters, imageSize, imageSize, numChannels, Some(whitener), true)
+    // TODO(stephentu): currently not flexible
+    assert(convolver.resWidth == 27 && convolver.resHeight == 27)
+    val stride = 2
+    val windowSize = 19
+    val nResulting = (0 until convolver.resWidth - windowSize + 1 by stride)
+      .flatMap { _ => (0 until convolver.resHeight - windowSize + 1 by stride).map { _ => 1 } }
+      .size
+
+    println(s"convolver.resWidth ${convolver.resWidth} convolver.resHeight ${convolver.resHeight}")
+    println(s"nResulting $nResulting")
+
+    val pooling = SymmetricRectifier(alpha=conf.alpha)
         .andThen(new Pooler(conf.poolStride, conf.poolSize, identity, _.sum))
         .andThen(ImageVectorizer)
         .andThen(new Cacher[DenseVector[Double]])
+    val trainFeaturesUnscaled: RDD[DenseVector[Double]] = pooling(new Windower(stride, windowSize).apply(convolver(trainImages)))
+    val scaler = (new StandardScaler).fit(trainFeaturesUnscaled).andThen(new Cacher[DenseVector[Double]])
 
-    val featurizer = unscaledFeaturizer.andThen(new StandardScaler, trainImages)
-        .andThen(new Cacher[DenseVector[Double]])
+    val trainFeatures = scaler(trainFeaturesUnscaled)
+    val trainLabels = new LabelAugmenter(nResulting).apply(LabelExtractor.apply(trainData)).cache()
 
-    val labelExtractor = LabelExtractor andThen new Cacher[Int]
-
-    val trainFeatures = featurizer(trainImages)
-    val trainLabels = labelExtractor(trainData)
-
-    val testData = CifarLoader(sc, conf.testLocation)
+    val testData = loader(sc, conf.testLocation)
     val testImages = ImageExtractor(testData)
 
     // gotta love spark
@@ -69,8 +92,12 @@ object RandomPatchCifarFeaturizer extends Serializable with Logging {
       label + "," + data.mkString(",")
     }.saveAsTextFile(conf.trainOutfile)
 
-    val testFeatures = featurizer(testImages)
-    val testLabels = labelExtractor(testData)
+    val testFeatures = convolver
+        .andThen(new ImageCrop(4, 4, windowSize + 4, windowSize + 4))
+        .andThen(pooling)
+        .andThen(scaler)
+        .apply(testImages)
+    val testLabels = LabelExtractor.andThen(new Cacher[Int]).apply(testData)
 
     testLabels.zip(testFeatures.map(_.toArray)).map { case (label, data) =>
       label + "," + data.mkString(",")
@@ -88,7 +115,8 @@ object RandomPatchCifarFeaturizer extends Serializable with Logging {
       patchSteps: Int = 1,
       poolSize: Int = 14,
       poolStride: Int = 13,
-      alpha: Double = 0.25)
+      alpha: Double = 0.25,
+      imageSize: Int = 32)
 
   def parse(args: Array[String]): RandomCifarFeaturizerConfig = new OptionParser[RandomCifarFeaturizerConfig](appName) {
     head(appName, "0.1")
@@ -102,6 +130,7 @@ object RandomPatchCifarFeaturizer extends Serializable with Logging {
     opt[Int]("patchSteps") action { (x,c) => c.copy(patchSteps=x) }
     opt[Int]("poolSize") action { (x,c) => c.copy(poolSize=x) }
     opt[Double]("alpha") action { (x,c) => c.copy(alpha=x) }
+    opt[Int]("imageSize") action { (x,c) => c.copy(imageSize=x) }
   }.parse(args, RandomCifarFeaturizerConfig()).get
 
   /**
