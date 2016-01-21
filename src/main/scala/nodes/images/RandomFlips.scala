5a42b6cf63d278f359ef4c5a0f6afc38437637b7
Shivaram Venkataraman <shivaram@eecs.berkeley.edu>
2016-01-21 13:20:40 -0800

    Flip with 50% probability. Add a centerCorner mode

--- src/main/scala/nodes/images/RandomFlips.scala
+++ src/main/scala/nodes/images/RandomFlips.scala
@@ -10,10 +10,17 @@ import utils.{ImageMetadata, ChannelMajorArrayVectorizedImage, Image}
  * @param numPatches Number of random patches to create for each image.
  * @param windowDim Dimension of the window (24 for CIFAR)
  */
-class RandomFlips(numPatches: Int, windowDim: Int) extends FunctionNode[RDD[Image], RDD[Image]] {
+class RandomFlips(
+    numPatches: Int,
+    windowDim: Int,
+    centerCorners: Boolean = false) extends FunctionNode[RDD[Image], RDD[Image]] {
 
   def apply(in: RDD[Image]) = {
-    in.flatMap(getCroppedImages)
+    if (centerCorners) {
+      in.flatMap(getCroppedTestImages)
+    } else {
+      in.flatMap(getCroppedImages)
+    }
   }
 
   // This function will take in an image and return `numPatches` 24x24 random
@@ -24,9 +31,10 @@ class RandomFlips(numPatches: Int, windowDim: Int) extends FunctionNode[RDD[Imag
     val numChannels = image.metadata.numChannels
 
     val r = new scala.util.Random(123L)
-    (0 until numPatches).flatMap { x =>
+    (0 until numPatches).map { x =>
+      // Flip with 50% probability as in cuda-convnet
+      val flip = r.nextBoolean()
       val randomPatch = new DenseVector[Double](windowDim * windowDim * numChannels)
-      val randomPatchFlipped = new DenseVector[Double](windowDim * windowDim * numChannels)
       // Assume x and y border are same size, for CIFAR, borderSize=8=32-24
       val borderSize = xDim - windowDim
       // Pick a random int between 0 and borderSize (inclusive)
@@ -38,31 +46,30 @@ class RandomFlips(numPatches: Int, windowDim: Int) extends FunctionNode[RDD[Imag
       while (c < numChannels) {
         var s = startX
         while (s < endX) {
-          var b = startY
-          while (b < endY) {
-            randomPatch(c + (s-startX)*numChannels +
-              (b-startY)*(endX-startX)*numChannels) = image.get(s, b, c)
-            b = b + 1
-          }
-          // TODO: Is this the right axis to flip ?
-          var bSource = startY
-          var bDest = endY - 1
-          while (bDest >= startY) {
-            randomPatchFlipped(c + (s-startX)*numChannels +
-              (bDest-startY)*(endX-startX)*numChannels) = image.get(s, bSource, c)
-            bDest = bDest - 1
-            bSource = bSource + 1
+          if (!flip) {
+            var b = startY
+            while (b < endY) {
+              randomPatch(c + (s-startX)*numChannels +
+                (b-startY)*(endX-startX)*numChannels) = image.get(s, b, c)
+              b = b + 1
+            }
+          } else {
+            // TODO: Is this the right axis to flip ?
+            var bSource = startY
+            var bDest = endY - 1
+            while (bDest >= startY) {
+              randomPatch(c + (s-startX)*numChannels +
+                (bDest-startY)*(endX-startX)*numChannels) = image.get(s, bSource, c)
+              bDest = bDest - 1
+              bSource = bSource + 1
+            }
           }
           s = s + 1
         }
         c = c + 1
       }
-      Iterator(
-        ChannelMajorArrayVectorizedImage(randomPatch.toArray,
-          ImageMetadata(windowDim, windowDim, numChannels)),
-        ChannelMajorArrayVectorizedImage(randomPatchFlipped.toArray,
-          ImageMetadata(windowDim, windowDim, numChannels))
-      )
+      ChannelMajorArrayVectorizedImage(randomPatch.toArray,
+        ImageMetadata(windowDim, windowDim, numChannels))
     }
   }
   /*
