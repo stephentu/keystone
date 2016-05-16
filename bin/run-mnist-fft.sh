5b6c557f8dcdcb4f39a1b1058f6ed0f98c8074fb
Shivaram Venkataraman <shivaram@cs.berkeley.edu>
2016-05-16 14:52:11 -0700

    Working configs for MNIST

--- bin/run-mnist-fft.sh
+++ bin/run-mnist-fft.sh
@@ -4,8 +4,13 @@ SPARK_JAVA_OPTS="-Dspark.master=local[5]" KEYSTONE_MEM=4g ./bin/run-pipeline.sh 
   pipelines.images.mnist.MnistRandomFFT \
   --trainLocation ./train-mnist-dense-with-labels.data \
   --testLocation ./test-mnist-dense-with-labels.data \
-  --numFFTs 4 \
-  --lambda 0.1 \
-  --cocoaNumLocalItersFraction 0.1 \
+  --numPartitions 10 \
+  --lambda 1e-6 \
+  --cocoaNumLocalItersFraction 1.0 \
+  --cocoaBeta 1.0 \
+  --sgdMiniBatchFraction 1.0 \
+  --sgdStepSize 600.0 \
+  --numIters 20 \
   --solver cocoa \
-  --blockSize 2048
+  --numFFTs 1 \
+  --blockSize 512
