57d6fca3f640e254591b51e424162d1f461275b2
Shivaram Venkataraman <shivaram@cs.berkeley.edu>
2016-05-19 17:18:33 +0000

    Add scripts to run sgd

--- bin/run-cifar-sgd.sh
+++ bin/run-cifar-sgd.sh
@@ -18,17 +18,17 @@ CIFAR_TEST_DIR="s3n://cifar-augmented/cifar_test_featurized_augmented_512_flip"
 
 NUM_PARTITIONS=1024
 LAMBDAS=1e-6
-NUM_ITERS=300
+NUM_ITERS=500
 NUM_COSINES=202752
 BLOCK_SIZE=6144
 GAMMA=0.00707
 SEED=123123
 SOLVER="sgd"
-NORM_STD="false"
 LOG_SUFFIX=`date +"%Y_%m_%d_%H_%M_%S"`
 #MAX ROW NORM IS 287.31838646475273
-STEP_SIZE=3e-2
-MINI_BATCH_FRACTION=1.0
+STEP_SIZE=1e-3
+SGD_DAMPEN=0.95
+MINI_BATCH_FRACTION=0.1
 COCOA_BETA=1.0
 COCOA_LOCAL_ITERS=1.0
 
@@ -46,10 +46,10 @@ OMP_NUM_THREADS=1 KEYSTONE_MEM=180g ./bin/run-pipeline.sh \
   --numCosineFeatures $NUM_COSINES \
   --numIters $NUM_ITERS \
   --solver $SOLVER \
-  --normStd $NORM_STD \
+  --sgdDampen $SGD_DAMPEN \
   --stepSize $STEP_SIZE \
   --cocoaBeta $COCOA_BETA \
   --cocoaLocalItersFraction $COCOA_LOCAL_ITERS \
   --miniBatchFraction $MINI_BATCH_FRACTION \
-  --seed $SEED 2>&1 | tee /root/logs/cifar-512-solver-$SOLVER-gamma-$GAMMA-lambda-$LAMBDAS-numIter-$NUM_ITERS-numCosine-$NUM_COSINES-normStd-$NORM_STD-step-$STEP_SIZE-mini-$MINI_BATCH_FRACTION-logs-"$LOG_SUFFIX".log
+  --seed $SEED 2>&1 | tee /root/logs/cifar-512-solver-$SOLVER-gamma-$GAMMA-lambda-$LAMBDAS-numIter-$NUM_ITERS-numCosine-$NUM_COSINES-step-$STEP_SIZE-mini-$MINI_BATCH_FRACTION-dampen-$SGD_DAMPEN-logs-"$LOG_SUFFIX".log
 
---
+++ bin/run-yelp-sgd.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+set -x
+
+FWDIR="$(cd `dirname $0`/..; pwd)"
+pushd $FWDIR
+
+export SPARK_HOME="/root/spark"
+source /root/spark/conf/spark-env.sh
+
+export MEM=180g
+
+MASTER=`cat /root/spark-ec2/cluster-url`
+CLASS=YelpHashLBFGS
+
+TRAIN_FEATURES="s3n://yelp-rating-reviews/yelp_academic_dataset_review-train-tokens.txt"
+#TRAIN_LABELS="/root/stephentu-keystone/yelp_academic_dataset_review-train-labels.txt"
+TRAIN_LABELS="s3n://yelp-rating-reviews/yelp_academic_dataset_review-train-labels.txt"
+TEST_FEATURES="s3n://yelp-rating-reviews/yelp_academic_dataset_review-test-tokens.txt"
+#TEST_LABELS="/root/stephentu-keystone/yelp_academic_dataset_review-test-labels.txt"
+TEST_LABELS="s3n://yelp-rating-reviews/yelp_academic_dataset_review-test-labels.txt"
+
+SOLVER=sgd
+LAMBDA=7.9655e-8
+NUM_HASH_FEATURES=122880
+BLOCK_SIZE=6144
+NUM_ITERS=300
+SEED=1213279863
+NUM_PARTITIONS=1024
+TEST_PARTITIONS=1024
+LOG_SUFFIX=`date +"%Y_%m_%d_%H_%M_%S"`
+#1e-3 is too slow
+#1e1 is too slow
+#1e3 iter 8 is 1.572068 
+#2.5e3 diverges
+#5e3 diverges
+#1e4 diverges
+#1e5 diverges
+STEP_SIZE=1.25e3
+SGD_DAMPEN=0.95
+MINI_BATCH_FRACTION=0.1
+COCOA_BETA=1.0
+COCOA_LOCAL_ITERS=1.0
+
+export EXECUTOR_OMP_NUM_THREADS=1
+
+OMP_NUM_THREADS=1 KEYSTONE_MEM=180g ./bin/run-pipeline.sh \
+  pipelines.text.$CLASS \
+  --trainDataLocation $TRAIN_FEATURES \
+  --trainLabelsLocation $TRAIN_LABELS \
+  --testDataLocation $TEST_FEATURES \
+  --testLabelsLocation $TEST_LABELS \
+  --trainParts $NUM_PARTITIONS \
+  --testParts $TEST_PARTITIONS \
+  --numHashFeatures $NUM_HASH_FEATURES \
+  --numIters $NUM_ITERS \
+  --blockSize $BLOCK_SIZE \
+  --lambda $LAMBDA \
+  --solver $SOLVER \
+  --sgdDampen $SGD_DAMPEN \
+  --stepSize $STEP_SIZE \
+  --cocoaBeta $COCOA_BETA \
+  --cocoaLocalItersFraction $COCOA_LOCAL_ITERS \
+  --miniBatchFraction $MINI_BATCH_FRACTION \
+  --seed $SEED 2>&1 | tee /root/logs/yelp-solver-$SOLVER-lambda-$LAMBDA-numHash-$NUM_HASH_FEATURES-numIters-$NUM_ITERS-mini-$MINI_BATCH_FRACTION-step-$STEP_SIZE-dampen-$SGD_DAMPEN-logs-"$LOG_SUFFIX".log
+
